ac240080074c0dbcdc875516a52ba858324304c4
oreolion <remyoreo11@gmail.com>
2024-05-26 04:04:30 -0700

    Add protected routes and database;

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -1,9 +1,21 @@
 "use client";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils";
+
 
 const MemberClient = () => {
+    const {data : session} = useSession({
+        required: true,
+        onUnauthenticated () {
+            redirect("/api/auth/signin?callbackUrl=/client")
+        }
+    })
   return (
     <div>
       <h1>Member Client Session</h1>
+      <p>{session?.user?.email  }</p>
+      <p>{session?.user?.role  }</p>
+
     </div>
   );
 };
--- app/member/page.jsx
+++ app/member/page.jsx
@@ -1,7 +1,22 @@
-const Member = () => {
+import { getServerSession } from "next-auth";
+import {options}  from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
+
+
+const Member =async () => {
+    const session = await getServerSession(options);
+
+   if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/member")
+   }
+ 
+
+
   return (
     <div>
       <h1>MEMBER SERVER SESSION</h1>
+      <p>{session?.user?.email  }</p>
+      <p>{session?.user?.role  }</p>
     </div>
   );
 };
